e4a5fa12c585f9d3a1a90a308bda4682a1d8923a
AnaLuisPereira <analuisfcpereira@gmail.com>
2020-11-23 23:02:48 +0000

    Added Forecast

--- src/app.js
+++ src/app.js
@@ -11,6 +11,17 @@ function formatDate(timestamp){
     return `${day}, ${hours}:${minutes}`;
 }
 
+function formatHours(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10) { hours = `0${hours}`
+        };
+    let minutes = date.getMinutes();
+    if (minutes<10) { minutes = `0${minutes}`
+        }; 
+    return `${hours}:${minutes}`;
+}
+
 function displayTemperature (response){
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
@@ -31,10 +42,40 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 iconElement.setAttribute("alt", response.data.weather[0].description); 
 }; 
 
+function displayForecast(response){
+    let forecastElement = document.querySelector("#forecast"); 
+    forecastElement.innerHTML = null; 
+    let forecast = null; 
+   
+    for (let index = 0; index < 6; index++) {
+         let forecast = response.data.list[index]; 
+        forecastElement.innerHTML +=`
+     <div class="col-2">
+            <h5> 
+            ${formatHours(forecast.dt*1000)} 
+            </h5>
+            <img 
+            src= http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png 
+            alt= ${forecast.weather[0].description}
+            />
+            
+            <div class="weather-forecast-temperature">
+              <strong> ${Math.round(forecast.main.temp_max)}º </strong> 
+              ${Math.round(forecast.main.temp_min)
+            }º
+            </div>
+          </div>
+    `;   
+    }
+}
+
 function search (city){
 let apiKey = "830aed891211b6be1ef87627b8211da0";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
 axios.get(apiUrl).then(displayTemperature); 
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; 
+axios.get(apiUrl).then(displayForecast); 
 }
 
 function handleSubmit(event){
